cfcd924591ba9112dbf7be7e80c83cfacf807ebc
CalliopeVanrogue <harleyquinn771@gmail.com>
2024-11-11 15:30:20 -0800

    Final touches added

--- Assets/Scripts/PrefabGenerator.cs
+++ Assets/Scripts/PrefabGenerator.cs
@@ -5,10 +5,18 @@ using UnityEngine;
 public class PrefabGenerator : MonoBehaviour
 {
     public GameObject prefab;
+    public float spacing = 2.0f;
     // Start is called before the first frame update
     void Start()
     {
-        
+          for (int i = 0; i < 10; i++)
+        {
+            // Calculate the position for each street lamp
+            Vector3 position = transform.position + new Vector3(i * spacing, 0, 0);
+            
+            // Instantiate the street lamp at the calculated position
+            Instantiate(prefab, position, transform.rotation);
+        }  
     }
 
     // Update is called once per frame
